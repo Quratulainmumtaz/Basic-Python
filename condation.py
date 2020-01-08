8cb114d3ac4845ad5718ea562fce74cc41d7051a
Qurat Ul Ain Mumtaz <mcs.qurat@gmail.com>
2020-01-08 22:26:29 +0500

    add condation.py , variable.py

---
+++ condation.py
@@ -0,0 +1,4 @@
+x = float(input("Enter any first number="))
+y = float(input("Enter any 2nd number="))
+z = float(input("Enter any 3rd number="))
+print("the maximum number is :", max(x, y, z))
---
+++ variable.py
@@ -0,0 +1,23 @@
+a=10
+print(a)
+b = 100.0
+print(b)
+city = "Karchi"
+print(city)
+d = 30
+student_name = "Qurat Ul Ain"
+father_name = "Mumtaz Ahmad"
+print(student_name+father_name)
+a = 100
+b = 200
+sum = a+b
+print(sum)
+x = 50
+y = 30
+print("{0} * {1} = {2}" .format(x, y, x * y))
+print("Hellow" , "World" , sep="------------")
+name = "Max"
+print("Hellow %s" % name)
+age = 22
+print("Hellow %s ! are you %d year old" %(name , age))
+
